953f4f4d449b90a378c5629fd9ce963267b181f5
Кирилл Гурьев <kirillgurev@MacBook-Pro-Kirill.local>
2023-02-21 23:19:37 +0300

    feat: add animation in result game

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -1,4 +1,4 @@
-import throttle from "lodash/throttle";
+import throttle from 'lodash/throttle';
 
 export default class FullPageScroll {
   constructor() {
@@ -6,22 +6,19 @@ export default class FullPageScroll {
     this.scrollFlag = true;
     this.timeout = null;
 
-    this.screenElements = document.querySelectorAll(
-      `.screen:not(.screen--result)`
-    );
-    this.menuElements = document.querySelectorAll(
-      `.page-header__menu .js-menu-link`
-    );
+    this.screenElements = document.querySelectorAll(`.screen:not(.screen--result)`);
+    this.menuElements = document.querySelectorAll(`.page-header__menu .js-menu-link`);
 
-    this.activeScreen = 0;
     this.onScrollHandler = this.onScroll.bind(this);
     this.onUrlHashChengedHandler = this.onUrlHashChanged.bind(this);
+
+    this.addAnimationScreen();
   }
 
   init() {
     document.addEventListener(
       `wheel`,
-      throttle(this.onScrollHandler, this.THROTTLE_TIMEOUT, { trailing: true })
+      throttle(this.onScrollHandler, this.THROTTLE_TIMEOUT, { trailing: true }),
     );
 
     window.addEventListener(`popstate`, this.onUrlHashChengedHandler);
@@ -49,9 +46,46 @@ export default class FullPageScroll {
     }, this.THROTTLE_TIMEOUT);
   }
 
+  createChildSpanWithAnomationText(letter, duration, delay) {
+    return `<span style="transition-duration: ${duration}ms; transition-delay: ${delay}ms;">${letter}</span>`;
+  }
+
+  addAnimationScreen() {
+    // const stringsArray = text.split(` `);
+    const animatedText = document.querySelectorAll(`.animated_text`);
+    const timeDelay = [210, 140, 70];
+    const timeDuration = 500;
+    animatedText.forEach((node) => {
+      node.innerHTML = node.innerHTML
+        .split(` `)
+        .map((item) => {
+          let currentTimeDelayPosition = 0;
+          return `<span>
+        ${item
+          .split(``)
+          .map((letter) => {
+            if (currentTimeDelayPosition > timeDelay.length - 1) {
+              currentTimeDelayPosition = 0;
+            }
+            const mapLetter = this.createChildSpanWithAnomationText(
+              letter,
+              timeDuration,
+              timeDelay[currentTimeDelayPosition],
+            );
+
+            currentTimeDelayPosition++;
+
+            return mapLetter;
+          })
+          .join(``)}
+        </span>`;
+        })
+        .join(``);
+    });
+  }
   onUrlHashChanged() {
     const newIndex = Array.from(this.screenElements).findIndex(
-      (screen) => location.hash.slice(1) === screen.id
+      (screen) => location.hash.slice(1) === screen.id,
     );
     this.activeScreen = newIndex < 0 ? 0 : newIndex;
     this.changePageDisplay();
@@ -82,9 +116,7 @@ export default class FullPageScroll {
     if (this.activeScreen === 2) {
       this.screenElements[this.activeScreen].classList.add(`active`);
       setTimeout(() => {
-        this.screenElements[this.activeScreen].classList.remove(
-          `screen--hidden`
-        );
+        this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
       }, 500);
     } else {
       this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
@@ -96,7 +128,7 @@ export default class FullPageScroll {
 
   changeActiveMenuItem() {
     const activeItem = Array.from(this.menuElements).find(
-      (item) => item.dataset.href === this.screenElements[this.activeScreen].id
+      (item) => item.dataset.href === this.screenElements[this.activeScreen].id,
     );
     if (activeItem) {
       this.menuElements.forEach((item) => item.classList.remove(`active`));
@@ -118,10 +150,7 @@ export default class FullPageScroll {
 
   reCalculateActiveScreenPosition(delta) {
     if (delta > 0) {
-      this.activeScreen = Math.min(
-        this.screenElements.length - 1,
-        ++this.activeScreen
-      );
+      this.activeScreen = Math.min(this.screenElements.length - 1, ++this.activeScreen);
     } else {
       this.activeScreen = Math.max(0, --this.activeScreen);
     }
